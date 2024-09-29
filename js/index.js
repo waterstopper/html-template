393a101df2f2932f2c53f47d9b152981e30b020a
waterstop <waterstop@yandex-team.ru>
2024-09-29 18:59:20 +0300

    update styles and add more elements

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+import { changeTheme } from "./loader.js";
 import { byId } from "./utils.js";
 
 console.log("Hello");
+
+byId("main-title").onclick = () => {
+    console.log("Change")
+    changeTheme()
+}
--- js/loader.js
+++ js/loader.js
@@ -1,1 +1,14 @@
 document.documentElement.setAttribute("data-theme", "light");
+
+function changeTheme() {
+    let theme = document.documentElement.getAttribute("data-theme");
+    if (theme == "light") {
+        theme = "dark"
+    } else {
+        theme = "light"
+    }
+    localStorage.setItem("data-theme", theme)
+    document.documentElement.setAttribute("data-theme", theme);
+}
+
+export { changeTheme }
